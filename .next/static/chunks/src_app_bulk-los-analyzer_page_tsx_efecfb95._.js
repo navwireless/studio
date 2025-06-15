9ff3d51214da27a3f756617af4a8ce251a80126d
Raj Patel <rknavtechno@gmail.com>
2025-06-15 00:58:27 +0000

    commit

---
+++ .next/static/chunks/src_app_bulk-los-analyzer_page_tsx_efecfb95._.js
@@ -0,0 +1,20 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_bulk-los-analyzer_page_tsx_efecfb95._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_bulk-los-analyzer_page_tsx_efecfb95._.js",
+  "chunks": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_fab71633._.js",
+    "static/chunks/src_6f79fddf._.js",
+    "static/chunks/node_modules_zod_lib_index_mjs_ee760afb._.js",
+    "static/chunks/node_modules_next_78f4edac._.js",
+    "static/chunks/node_modules_jszip_lib_e13ccca1._.js",
+    "static/chunks/node_modules_pako_e267515d._.js",
+    "static/chunks/node_modules_xlsx_xlsx_mjs_ad755052._.js",
+    "static/chunks/node_modules_lodash_5abeef98._.js",
+    "static/chunks/node_modules_recharts_es6_1ecb0bd0._.js",
+    "static/chunks/node_modules_224ba80b._.js"
+  ],
+  "source": "dynamic"
+});
